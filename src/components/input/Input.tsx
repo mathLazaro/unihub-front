dc920fac52b1ba87f7fa6729c5ec02699067b34e
Matheus Lázaro <matheus.lazaro2002@gmail.com.br>
2026-06-15 02:58:37 -0300

    feat: input padrão

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,58 @@
+import type { InputHTMLAttributes } from "react";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder: string;
+  type?: string;
+  value: string;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+  width?: string;
+
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function Input({
+  placeholder,
+  type = "text",
+  value,
+  width = "100%",
+  icon,
+  iconPosition = "left",
+  onChange,
+}: InputProps) {
+  const wraperClass = `p-2 border border-gray-300 rounded-full focus-within:ring-2 focus-within:ring-primary bg-[#F5F6FA] flex items-center gap-2 px-4`;
+
+  const inputTemplate = (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="flex-1 focus:outline-none bg-transparent"
+    />
+  );
+
+  if (icon) {
+    switch (iconPosition) {
+      case "left":
+        return (
+          <div style={{ width }} className={wraperClass}>
+            {icon}
+            {inputTemplate}
+          </div>
+        );
+      case "right":
+        return (
+          <div style={{ width }} className={wraperClass}>
+            {inputTemplate}
+            {icon}
+          </div>
+        );
+    }
+  }
+  return (
+    <div style={{ width }} className={wraperClass}>
+      {inputTemplate}
+    </div>
+  );
+}
